d3cfd5b20ccb849d1744853f28cd752280ba103e
Shubham <shub.saini003@gmail.com>
2018-04-16 16:32:16 +0530

    c1

--- app/src/main/java/com/example/shubham/sespro/MainActivity.java
+++ app/src/main/java/com/example/shubham/sespro/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.shubham.sespro;
 
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import com.android.volley.Request;
@@ -25,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         Button spt = (Button) findViewById(R.id.SpeechToText);
         spt.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
         });
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://www.google.com";
+        Button I2M = (Button) findViewById(R.id.ImageToMp3);
+        I2M.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //       .setAction("Action", null).show();
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 1);
+
+            }
+        });
 
 // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
